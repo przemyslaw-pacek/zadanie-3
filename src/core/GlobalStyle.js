5cbc97b9ae6428790fc81d7dece5bd54ab3b1368
Przemyslaw Pacek <ppacek@op.pl>
2025-11-06 23:17:37 +0100

    added GlobalStyle

---
+++ src/core/GlobalStyle.js
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+    box-sizing: border-box;
+  }
+  
+  *,
+  ::after ::before {
+    box-sizing: inherit;
+  }
+  
+  body { 
+    color: white;
+    background: #222;    
+    max-width: 1000px;
+    margin: 0 auto;
+  }
+`;
